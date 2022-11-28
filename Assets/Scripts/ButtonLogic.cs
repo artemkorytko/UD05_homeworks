1155d4323de43ae2db2395aba4609d57250fe354
HOME-PC\JUST <roman.raven96@mail.ru>
2022-11-28 21:41:54 +0300

    Do Calculator. Still not working...

---
+++ Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,64 @@
+using System;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonLogic : MonoBehaviour
+{
+    [SerializeField] private Text showText;
+    [SerializeField] private string value;
+    private string textString;
+    private char[] separators = {'+', '-', '*', '/', '^'};
+    private string[] splitText;
+    private bool enterSign = false;
+    private char sign;
+    [SerializeField] private double x;
+    [SerializeField] private double y;
+    
+    public void OnClickButton()
+    {
+        if (value != "+" && value != "-" && value != "*" && value != "/" && value != "^")
+        {
+            showText.text += value;
+            
+        }
+        else if (enterSign == false)
+        {
+            sign = Convert.ToChar(value);
+            x = Convert.ToDouble(showText.text);
+            showText.text += "\n";
+            showText.text += value;
+            showText.text += "\n";
+            enterSign = true;
+        }
+        else if (enterSign)
+        {
+            showText.text += "\nСотри всё и введи выражение из двух частей!";
+        }
+    }
+    
+    public void ClearAll()
+    {
+        showText.text = null;
+        enterSign = false;
+    }
+    
+    public void Clear()
+    {
+        if (showText != null)
+        {
+            textString = showText.text;
+            textString = textString.Remove(textString.Length - 1);
+            showText.text = textString;
+        }
+    }
+
+    public void OnClickCalc()
+    {
+        splitText = showText.text.Split(separators);
+        y = Convert.ToDouble(splitText[1]);
+        Calculation Calc = new Calculation(x, y, sign);
+        showText.text = Calc.Calculate();
+        enterSign = false;
+    }
+}
---
+++ Assets/Scripts/Calculation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DefaultNamespace
+{
+    public class Calculation
+    {
+        private double _x;
+        private double _y;
+        private double _result;
+        private char _sign;
+        private bool _error;
+        private string _answer;
+        
+        public Calculation(double x, double y, char sign)
+        {
+            _x = x;
+            _y = y;
+            _sign = sign;
+        }
+        
+        public string Calculate()
+        {
+            _error = false;
+            if (_sign == '/' && _y == 0)
+            {
+                _error = true;
+            }
+            if (_error == false)
+            {
+                switch (_sign)
+                {
+                    case '+':
+                        _result = _x + _y;
+                        break;
+                    case '-':
+                        _result = _x - _y;
+                        break;
+                    case '*':
+                        _result = _x * _y;
+                        break;
+                    case '/':
+                        _result = _x / _y;
+                        break;
+                    case '^':
+                        _result = (Math.Pow(_x, _y));
+                        break;
+                }
+                
+                _answer = Convert.ToString(_result);
+            }
+            else
+            {
+                _answer = "На ноль делить нельзя!";
+            }
+
+            return _answer;
+        }
+    }
+}
