964f98adbf9a7c377dc9e0e119257f7838ce1be3
Pinksharkapps <pinksharkapps@gmail.com>
2022-11-28 09:03:12 +0300

    font fixed ("text mesh pro" asset cretor, not "text"), UI ready

---
+++ Assets/Scripts/FieldController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string Value1 = "?";
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
