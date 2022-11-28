8dd5125d8c2c985e9fde40feadd00a36fe1381d1
IFursa99 <IfursaStudy@yandex.com>
2022-11-28 23:14:47 +0300

    create culc, add +

---
+++ Assets/Scripts/ControllerCulc.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ControllerCulc : MonoBehaviour
+    {
+        public TextMeshProUGUI result;
+        private int x, y;
+        private char Prosto;
+
+
+        public void AddSign(ButtonData data)
+        {
+            result.text += data.Sign;
+        }
+
+        public void Clear()
+        {
+            result.text = "";
+        }
+
+        public void Plus()
+        {
+            Prosto = '+';
+            x = int.Parse(result.text);
+            Clear();
+
+        }
+
+        public void Result()
+        {
+
+            y = int.Parse(result.text);
+            switch (Prosto)
+            {
+                case '+':
+                    result.text = (x + y).ToString();
+                    break;
+
+
+            }
+
+
+        }
+
+        public void Result1()
+        {
+
+            y = int.Parse(result.text);
+            switch (Prosto)
+            {
+                case '/':
+                    result.text = (x / y).ToString();
+                    break;
+
+
+            }
+        }
+    }
+}
